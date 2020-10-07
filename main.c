f4b53b034f73b5a6c73edd978c215a7c3dae7635
Arm <armoop01@gmail.com>
2020-10-07 08:38:08 +0000

    0.1.1.2

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
 int main(void) {
   int num,i,j,m=0,flag=0;
 
-do{      
+while(num >= 0)  {      
 printf("Enter number : ");    
 scanf("%d",&num);
 
  if((num==1 || num%2 == 0 || num%3 == 0 || num%5==0 || num%7==0) 
             && (num!=2 && num!=3 && num!=5 && num!=7))  
   {    
-  printf("Not prime number!!!\n");        
+  printf("Not prime number!!!\n"); continue;       
   }    
       
-  else{
+  if(num>0){
   for(j=1;j<13;j++)
        printf("%d x %d = %d\n",num,j,j*num);   } 
-}while(num >= 0);      
+  continue;
+}    
 
 
   return 0;
