03778d67b6ce8368d392ef9682831395da166b07
Arm <armoop01@gmail.com>
2020-10-08 12:26:44 +0000

    0.1.1.4

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ printf("Enter number : ");
 scanf("%d",&num);
 
  if((num==1 || num%2 == 0 || num%3 == 0 || num%5==0 || num%7==0) 
-            && (num!=2 && num!=3 && num!=5 && num!=7))  
+            && (num!=2 && num!=3 && num!=5 && num!=7)&&(num >0))  
   {    
   printf("Not prime number!!!\n"); continue;       
   }    
